048447e14c285532e607f46f9493732dcaef4151
ayoub ouabi <ayoub.ouabi06@gmail.com>
2023-10-17 15:57:53 +0100

    Still working on login system

---
+++ src/main/java/com/ayoub/resourciumoptima/interfaces/EmployeeRepository.java
@@ -0,0 +1,20 @@
+package com.ayoub.resourciumoptima.interfaces;
+
+import com.ayoub.resourciumoptima.entities.Employee;
+
+import java.util.List;
+
+public interface EmployeeRepository {
+
+    void save(Employee obj);
+
+    void delete(Long id);
+
+    void update(Employee obj);
+
+    Employee findById(Long id);
+
+    List<Employee> getAll();
+
+    public Employee findByEmailAndPassword(  String email,String password );
+}
